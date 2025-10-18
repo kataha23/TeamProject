b0a280044c602ffb613e2d9f58820b31306fc1b8
kataha23 <melong3023@gmail.com>
2025-10-18 17:37:40 +0900

    2025/10/18 2D 팔레트로 레벨 제작

---
+++ Topd/Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 페이드 아웃
+
+        // 페이드 인
+
+        // 이동
+        // CinemachineConfier2D -> BoundinngShape A1 -> a1-1
+        // player.transform.position.2D -> 집으로!
+    }
+}
